16a020ae2ce95e332572e252961e3289498d7668
kvachonoky-creator <kvachonoky@gmail.com>
2025-12-18 11:06:20 +0300

    destruct

---
+++ src/07/07.ts
@@ -0,0 +1,11 @@
+
+const props = {
+    username: "Dimych",
+    age: 32,
+    lessons: [{ title: "1" }, { title: "2" }]
+}
+
+const { username, age, lessons } = props
+
+
+console.log(username, age, lessons)
---
+++ src/07/Desctucturing.tsx
@@ -0,0 +1,34 @@
+import React from "react"
+
+type ManType = {
+    name: string
+    age: number
+    lessons: Array<{ title: string }>
+    address: {
+        street: {
+            title: string
+        }
+    }
+}
+
+
+type PropsType = {
+    title: string
+    man: ManType
+    food: Array<string>
+    car: {model: string}
+}
+
+export const ManComponent: React.FC<PropsType> = ({title, man, ...props}) => {
+
+    return <div>
+        <h1>{title}</h1>
+        <hr />
+        <div>
+            {man.name}
+        </div>
+        <div>
+            {props.car.model}
+        </div>
+    </div>
+}
